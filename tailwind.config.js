e0fc72876162d1c369b7a240dc63c6dbbb25298a
silva4dev <web4dev.lucas@outlook.com>
2024-03-24 19:17:25 -0300

    chore: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: [
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.{js,ts,jsx,tsx}",
+    "./app/views/**/*.{erb,haml,html,slim}",
+  ],
+};
